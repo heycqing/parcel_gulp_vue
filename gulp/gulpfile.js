b3bb313b05b7300e344d6a01d77b8032939aea2a
SiqingIII@163.com <cqing@cqing.local>
2018-01-30 16:16:56 +0800

    cqing
    gulp-minify-html

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var imgsmin = require('gulp-imagemin')
-
+var htmlmin = require('gulp-minify-html')
 
 gulp.task('imgmin',function(){
-    console.log('images minfy');
+    console.log('images minify');
     gulp.src('../static/imgs/*')
     .pipe(imgsmin())
     .pipe(gulp.dest('../build/imgs'))
     
 })
 
-gulp.task('default',['imgmin'])
+gulp.task('htmlmin',function(){
+    console.log('html minify');
+    gulp.src('../*.html')
+    .pipe(htmlmin())
+    .pipe(gulp.dest('../build/html'));
+})
+
+gulp.task('default',['imgmin','htmlmin']);
